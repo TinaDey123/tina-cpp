5ed7c2547b2e94120c399fc4c9878762c4b3f0aa
Tina Dey <deyt46333@gmail.com>
2026-08-05 23:04:22 +0530

    Added program2.c

--- tina.c
+++ tina.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("enter a number");
-    scanf("%d",&n);
-    for(int i=1;i<n;i++)
-    {
-        printf("DSA\n");
-    }
-
-return 0;
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   for(int i=1;i<=n;i++)
+   {
+     printf("DSA\n");
+   }
+   return 0;
 }
