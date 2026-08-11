41a6320f93bd99891728de868c283150236a1a9b
Tina Dey <deyt46333@gmail.com>
2026-08-11 20:33:03 +0530

    program7

---
+++ program7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={12,4,5,90,45};
+    int b[2][3]={{2,4,5},{7,9,3}};
+    printf("1D arra:%d\n",a[2]);
+    printf("2D array %d\n",b[1][2]);
+    return 0;
+}
